21aa9a70c5402523dd7a199787f608ae3a3644c1
Hamza Bashir <hamzabashir218@gmail.com>
2024-02-22 16:56:16 +0500

    Products api added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BookModule, ProductModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
