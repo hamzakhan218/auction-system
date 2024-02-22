75348c05ffcd60c30ab45637640a1d835f404e5b
Hamza Bashir <hamzabashir218@gmail.com>
2024-02-22 17:44:41 +0500

    Added auth functionality.

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,27 +1,32 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('api/v1/product')
 export class ProductController {
     constructor(private readonly productService: ProductService){}
 
     @Get()
+    @UseGuards(JwtAuthGuard)
     async getAllProducts(){
         return this.productService.getAllProducts();
     }
 
     @Get(':id')
+    @UseGuards(JwtAuthGuard)
     async getUsersProducts(@Param('id') id: number){
         return this.productService.getUsersProducts(id);
     }
 
     @Post(':id')
+    @UseGuards(JwtAuthGuard)
     async createProduct(@Param('id') id: number, @Body() product){
         return this.productService.createProduct(id, product);
     }
 
     @HttpCode(204)
     @Delete(':id')
+    @UseGuards(JwtAuthGuard)
     async deleteProduct(@Param('id') id: number){
         return this.productService.deleteProduct(id);
     }
